f1dc1c7d04704bfda47398d17d425b4ad36050fc
BinaySaahu <binaysahu364@gmail.com>
2026-09-05 21:01:28 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,71 @@
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+from tensorflow.keras.models import load_model
+import pickle
+import streamlit as st
+
+model = load_model('model.h5')
+
+# Load scalar
+with open('scaler.pkl', 'rb') as file:
+    scaler = pickle.load(file)
+
+with open('onehot_encoder_geo.pkl', 'rb') as file:
+    onehot_encoder_geo = pickle.load(file)
+
+with open('label_encoder_gender.pkl', 'rb') as file:
+    label_encoder_gender = pickle.load(file)
+
+
+#App
+
+st.title('Customer Churn Prediction')
+
+
+
+credit_score = st.number_input('Credit Score')
+geography = st.selectbox('Geography', onehot_encoder_geo.categories_[0])
+gender = st.selectbox('Gender', label_encoder_gender.classes_)
+age = st.slider('Age', 18, 92)
+balance = st.number_input('Balance')
+estimated_salary = st.number_input('Estimated Salary')
+tenure = st.slider('Tenure', 0, 10)
+num_of_products = st.slider('Number of Products', 1, 4)
+has_cr_card = st.selectbox('Has Credit Card', [0, 1])
+is_active_member = st.selectbox('Is Active Member', [0, 1])
+
+
+
+def predict():
+    input_df=pd.DataFrame({
+        'CreditScore': [credit_score],
+        'Gender' : [label_encoder_gender.transform([gender])[0]],
+        'Age' : [age],
+        'Tenure' : [tenure],
+        'Balance' : [balance],
+        'NumOfProducts' : [num_of_products],
+        'HasCrCard' : [has_cr_card],
+        'IsActiveMember' : [is_active_member],
+        'EstimatedSalary': [estimated_salary]
+    })
+    geo_col = onehot_encoder_geo.transform([[geography]])
+    df = pd.DataFrame(geo_col.toarray(), columns=onehot_encoder_geo.get_feature_names_out(['Geography']))
+    input_df = pd.concat([input_df.reset_index(drop=True), df], axis=1)
+
+    inp_scaled = scaler.transform(input_df)
+
+    prediction=model.predict(inp_scaled)
+
+    prediction_probability = prediction[0][0]
+    st.write(prediction_probability)
+    if prediction_probability > 0.5:
+        st.write('The customer will leave')
+    else:
+        st.write("The customer won't leave")
+    
+
+if st.button("Predict Customer Churn"):
+    predict()
+
+
